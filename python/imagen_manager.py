f65eee1c8887c40ccbcce397c3d42aab225de593
ARaffo <49041262+ARaffo@users.noreply.github.com>
2021-06-10 17:20:08 -0300

    with imageio

--- python/imagen_manager.py
+++ python/imagen_manager.py
@@ -1,5 +1,7 @@
 import cv2
+import imageio as im
 import os
+import numpy as np
 import db_manager as dbm
 
 path_base = 'D:\\UTN\\5 año\\Gestion Avanzada de datos\\Tp-Final\\Imagenes\\Fotos Alumnos FRCU\\'
@@ -9,9 +11,19 @@ database = dbm.DatabaseConnection()
 
 for nombre in database.query_all_nombres()[:1]:
     path = os.path.join(path_base, nombre)
-    img = cv2.imread(path)
-    if img == None: 
-        print('error')
-        print(img)
-    print(path)
-   # cv2.imshow('hola',img)
+    img = im.imread(path)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # # if img == None: 
+    # #     print('error')
+    # #     print(img)
+    # # else:
+    # #     print('error')
+    # #     print(img)
+    # print(img)
+    # print(path)
+    #img = cv2.imread("0.jpg")
+    #print(img)
+    cv2.imshow('hola',img_rgb)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+   # print(type(path))
