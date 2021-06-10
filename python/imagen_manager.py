e0bb4dec319d9b3c1504d0a325ac0840a6df3693
ARaffo <49041262+ARaffo@users.noreply.github.com>
2021-06-10 18:26:18 -0300

    andando

--- python/imagen_manager.py
+++ python/imagen_manager.py
@@ -6,16 +6,25 @@ import db_manager as dbm
 from tensorflow.python import keras
 import Rostros_utils as utils
 import tensorflow as tf
+from psycopg2.extensions import register_adapter, AsIs
 
 
-model = tf.keras.models.load_model("""model path""")
-path_base = """path base a donde se encuentran las imagenes"""
+model = tf.keras.models.load_model(r'D:\UTN\5 año\Gestion Avanzada de datos\Tp-Final\Modelos\my_model3.h5')
+path_base = r'D:\UTN\5 año\Gestion Avanzada de datos\Tp-Final\Imagenes\Fotos Alumnos FRCU'
 
 database = dbm.DatabaseConnection()
 
-for nombre in database.query_all_nombres()[:1]:
+for nombre in os.listdir(path_base)[:1]:
     path = os.path.join(path_base, nombre)
-    img = im.imread(path)
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     embedding = utils.get_vector(path, model)
-    #db.insert_vector(nombre, embedding)
+    image = nombre,np.array(embedding).tolist()[0]
+    #print(np.array(embedding).tolist()[0])
+    database.insert_new_image(image)
+
+# def addapt_numpy_array(numpy_array):
+#     return AsIs(tuple(numpy_array))
+
+
+
+# img = im.imread(path)
+#  img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
