8c36936e0a0b10e219474dedc2617ffeaec79cfb
Nahuel <montesinonahuel@gmail.com>
2021-06-13 12:24:32 -0300

    feat: funcion para calcular distancia euclediana

---
+++ SQL/Distancia_euclediana.sql
@@ -0,0 +1,27 @@
+﻿
+CREATE OR REPLACE FUNCTION DistanciaEuclidiana(x float[], y float[]) RETURNS float AS --ej e (ver como devolver en diferentes campos)
+$$
+Declare
+sum float = 0;
+
+Begin
+for i in 1..array_length(x,1) LOOP
+
+sum = sum + power((x[i] - y[i]),2);
+
+
+END LOOP;
+
+
+
+return sqrt(sum);
+
+ 
+End
+$$
+language 'plpgsql'; 
+
+
+select DistanciaEuclidiana(array[3,4],array[0,0])
+
+select DistanciaEuclidiana('{1.25,2.45}'::float[],'{-3.56,1.67}'::float[])
